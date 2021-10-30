886efaff44d237d6d3e1f21a34bd4dd09643dd9f
mingcheng.zhou <mingcheng.zhou@shopee.com>
2021-10-30 21:45:24 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import React, { Component } from 'react';
+import { Button, List } from 'antd';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodName: '',
+      foodList: []
+    };
+  }
+
+  roll = (e) => {
+    axios.post('/roll', {})
+      .then(response => {
+        console.log(response);
+        if (response) {
+          this.setState({
+            foodName: response.data
+          })
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+
+  getList = (e) => {
+    axios.post('/list', {})
+      .then(response => {
+        console.log(response);
+        if (response) {
+          this.setState({
+            foodList: response.data.list
+          })
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>{this.state.foodName}</div>
+        <Button type="primary" onClick={this.roll}>roll</Button>
+        <Button type="primary" onClick={this.getList}>list</Button>
+        <List
+          // header={<div>Header</div>}
+          // footer={<div>Footer</div>}
+          bordered
+          dataSource={this.state.foodList}
+          renderItem={item => (
+            <List.Item>
+              {/* <Typography.Text mark>[ITEM]</Typography.Text> {item} */}
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
